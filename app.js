07d06e95970adf1b6bf8965d387f976280157e55
Oumamadanicherif <oumaimamc@gmail.com>
2022-05-21 20:27:54 +0100

    Commit APP.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var articlesRouter = require('./routes/articles');
+var categoriesRouter = require('./routes/categories');
+var commentairesRouter = require('./routes/commentaires');
 var app = express();
 
 // view engine setup
@@ -21,21 +23,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/users', articlesRouter);
+app.use('/users', categoriesRouter);
+app.use('/users', commentairesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
