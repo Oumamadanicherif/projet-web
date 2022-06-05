d55cd8c7e3f0879e290761654a7779c35b8151e1
Oumamadanicherif <oumaimamc@gmail.com>
2022-06-05 02:16:00 +0100

    methode d'authentification jwt

--- routes/login.js
+++ routes/login.js
@@ -1,18 +1,55 @@
 const express = require('express');
 var router = express.Router();
 const jwt = require('jsonwebtoken');
-const accessTokenSecret = 'madanicherif';
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-router.post('/', (req, res) => {
+const accessTokenSecret = 'najib';
+
+router.post('/', async(req, res) => {
     // Read username and password from request body
     const { username, password } = req.body;
 
     // Filter user from the users array by username and password
-    const user = users.find(u => { return u.username === username && u.password === password });
 
-    if (user) {
+    const usr = await prisma.utilisateur.findMany({
+        where: { email: username, password: password },
+    })
+
+    if (usr) {
         // Generate an access token
-        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret);
+        const accessToken = jwt.sign({ username: usr.email, role: usr.role }, accessTokenSecret);
+
+        res.json({
+            accessToken
+        });
+    } else {
+        res.send('Username or password incorrect');
+    }
+});
+
+
+module.exports = routerconst express = require('express');
+var router = express.Router();
+const jwt = require('jsonwebtoken');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const accessTokenSecret = 'madani cherif';
+
+router.post('/', async(req, res) => {
+    // Read username and password from request body
+    const { username, password } = req.body;
+
+    // Filter user from the users array by username and password
+
+    const usr = await prisma.utilisateur.findMany({
+        where: { email: username, password: password },
+    })
+
+    if (usr) {
+        // Generate an access token
+        const accessToken = jwt.sign({ username: usr.email, role: usr.role }, accessTokenSecret);
 
         res.json({
             accessToken
