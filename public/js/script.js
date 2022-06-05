56667aaa5a6596d445bd53bf68f45b270f5ad5a0
Oumamadanicherif <oumaimamc@gmail.com>
2022-06-05 02:19:39 +0100

    ajout deux fonctions

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,32 @@ $(document).ready(function() {
         $('body').toggleClass("down", (fromTop > 300))
     });
 });
+
+function getArticles(take = 10, skip = 0) {
+    fetch("http://localhost:3000/articles?take=10&skip=0").then(response =>
+        response.json().then(data => ({
+            data: data,
+            status: response.status
+
+        })).then(res => {
+            for (let index = 0; index < res.data.length; index++) {
+
+                document.getElementById("test").innerHTML += res.data[index].contenu;
+            }
+
+            console.log(res.status, res.data)
+        }));
+}
+
+function getCategories(take = 10, skip = 0) {
+    fetch("http://localhost:3000/categories?take=10&skip=0").then(response =>
+        response.json().then(data => ({
+            data: data,
+            status: response.status
+        })).then(res => {
+            console.log(res.status, res.data)
+        }));
+}
+
+
+getArticles(10, 0);
